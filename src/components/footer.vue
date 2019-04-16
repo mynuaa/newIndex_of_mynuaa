a9f94c2ce5e6b583b239f38e4b4b2e2766fa56ea
Seiry Yu <yuwenjie66@126.com>
2019-04-16 20:39:40 +0800

    分号！

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -53,13 +53,13 @@ console.log(s)
 console.log('%c', 'background:url(https://qzonestyle.gtimg.cn/qzone/em/e253.png)repeat-x;padding:15px 100% 20px 0;line-height:45px')
 
 console.log('%c如果有小可爱看到这个 可以投递简历到yuwenjie66#126.com哦 欢迎来纸飞机一起玩呀~', 'font-size:14px;font-family:"PingFang SC",微软雅黑,"WenQuanYi Micro Hei"')
-    var _hmt = _hmt || []
+    var _hmt = _hmt || [];
     (function() {
         var hm = document.createElement("script")
         hm.src = "https://hm.baidu.com/hm.js?f7c0295778ddd27005fadba73644ae21"
         var s = document.getElementsByTagName("script")[0]; 
         s.parentNode.insertBefore(hm, s);
-    })()
+    })();
 </script>
 
 
